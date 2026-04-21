f36eb18ae54fe4db0201a05ce75fc5ef32ff2a81
Muzikasi <muzikasidoyasi@gmai.com>
2026-04-21 14:03:18 +0300

    C++ if

--- C++/if statement.cpp
+++ C++/if statement.cpp
@@ -4,12 +4,17 @@ using namespace std;
 
 int main (){ 
    
-   int age = 18;
+   int age = 17;
    
    if(age >= 18){
    	
    	cout << "You are an adult";
+   }else{
+   	cout << "You are an child";
+   	
    }
+   
+   
 	
 	
 	return 0;
---
+++ C++/if workout.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+	
+	double score;
+	
+	
+	cout<<"Enter your Score: ";
+	cin>>score;
+	
+	if(score>=50){
+		cout<<"Pass!";
+	}else{
+		cout<<"Fail!";
+	}
+	
+	return 0;
+	
+}
