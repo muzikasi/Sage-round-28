b25a49526eb5a72beeb2a72dc134abd93af7c040
muzikasi <muzikasidoyasi@gmail.com>
2026-06-19 13:30:45 +0300

    deploy

--- Backend/Node Jwt Auth/index.js
+++ Backend/Node Jwt Auth/index.js
@@ -3,10 +3,11 @@ import mongoose from "mongoose";
 import bcrypt from 'bcrypt';
 import { User } from "./model/user.js";
 import jwt from "jsonwebtoken";
+import { Book } from "./model/book.js";
 const app = express()
 
 
-mongoose.connect('mongodb://localhost:27017/Auth')
+mongoose.connect('mongodb+srv://sage:ycYE7xUw9Nn941n6@cluster0.pzsqnpm.mongodb.net/auth?appName=Cluster0')
     .then(() => console.log("mongoose connected!"))
     .catch(err => console.log(err))
 
@@ -24,7 +25,8 @@ app.post('/signup', async (req, res) => {
         const newUser = new User({
             fullname: user.fullname,
             userName: user.userName,
-            password: hashedPassword
+            password: hashedPassword,
+            role:"user"
         })
         await newUser.save()
         return res.status(201).json({ message: "Account Created!" })
@@ -44,17 +46,73 @@ app.post('/login', async (req, res) => {
     if (!isMatch) {
         return res.status(400).json({ message: "incorrect username or password!" })
     }
-    const token = jwt.sign({ id: user.id, fullname: user.fullname, userName: user.userName, role: "admin" }, "2123")
+    const token = jwt.sign({ id: user.id, fullname: user.fullname, userName: user.userName, role: user.role }, "2123")
     return res.status(200).json({
         message: "Login Completed!",
         token: token
     })
 })
 
-app.get('/books', auth, (req, res) => {
-    return res.status(200).json(
-        { message: "list of books for authorized user. your name is: " + req.user.fullname }
-    )
+app.get('/books', auth, async (req, res) => {
+    try {
+        const book = await Book.find()
+        return res.status(200).json(book)
+    } catch (err) {
+        console.log(err)
+        return res.status(500).json({ message: "Internal Server Error" })
+    }
+})
+
+app.post('/books', auth, async (req, res) => {
+    try {
+        console.log(req.user)
+        if(req.user.role!= "admin"){
+            return res.status(400).json({message:"Not Permitted"})
+        }
+        const book = new Book({
+            title: req.body.title,
+            author: req.body.author,
+            price: req.body.price,
+        })
+        await book.save()
+        return res.status(201).json({ message: "Book Created!" })
+    } catch (err) {
+        console.log(err)
+        return res.status(500).json({ message: "Internal Server Error" })
+    }
+})
+
+
+app.put('/books/:id', auth, async (req, res) => {
+    try {
+        const existingBook = await Book.findById(req.params.id)
+        if (!existingBook) {
+            return res.status(404).json({ massage: "Book not found!" })
+        }
+        const book = await Book.findByIdAndUpdate(req.params.id,{ 
+            title: req.body.title,
+            author: req.body.author,
+            price: req.body.price
+        }, { new: true })
+        return res.status(200).json({ massage: "Book Updated!" })
+    } catch (err) {
+        console.log(err)
+        return res.status(500).json({ message: "Internal Server Error" })
+    }
+})
+
+app.delete('/books/:id', auth ,async (req, res) => {
+    try {
+        const book = await Book.findByIdAndDelete(req.params.id)
+         if (!book) {
+            return res.status(404).json({ message: "Book not found or already deleted!" });
+        }
+        return res.status(200).json({ massage: "Book Deleted and book name is: " + book.title })
+    } catch (err) {
+        console.log(err)
+        return res.status(500).json({ message: "Internal Server Error" })
+
+    }
 })
 
 function auth(req, res, next) {
--- Backend/Node Jwt Auth/model/user.js
+++ Backend/Node Jwt Auth/model/user.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     fullname:{type: String , required:true},
     userName:{type: String, require:true, unique: true},
-    password:{type: String, require: true}
+    password:{type: String, require: true},
+    role:{type:String}
 })
 
 export const User = mongoose.model('User', userSchema);
